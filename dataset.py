de9735209fffbadde3cab369adc66ba5f5b1b857
Degang Wang <wangdegang20@mails.ucas.ac.cn>
2024-05-11 20:34:51 +0800

    Add files via upload

--- dataset.py
+++ dataset.py
@@ -1,4 +1,3 @@
-import torch.nn.functional as F
 import torch
 import numpy as np
 import scipy.io as sio
@@ -22,49 +21,3 @@ def PDBSNetData(opt):
     print("The training dataloader construction process is done")
     print('-' * 50)
     return loader_train, band
-
-def pixel_shuffle_down_sampling(x:torch.Tensor, f:int, pad:int=0, pad_value:float=0.):
-    '''
-    pixel-shuffle down-sampling (PD) from "When AWGN-denoiser meets real-world noise." (AAAI 2019)
-    Args:
-        x (Tensor) : input tensor
-        f (int) : factor of PD
-        pad (int) : number of pad between each down-sampled images
-        pad_value (float) : padding value
-    Return:
-        pd_x (Tensor) : down-shuffled image tensor with pad or not
-    '''
-    # single image tensor
-    if len(x.shape) == 3:
-        c,w,h = x.shape
-        unshuffled = F.pixel_unshuffle(x, f)
-        if pad != 0: unshuffled = F.pad(unshuffled, (pad, pad, pad, pad), value=pad_value)
-        return unshuffled.view(c,f,f,w//f+2*pad,h//f+2*pad).permute(0,1,3,2,4).reshape(c, w+2*f*pad, h+2*f*pad)
-    # batched image tensor
-    else:
-        b,c,w,h = x.shape
-        unshuffled = F.pixel_unshuffle(x, f)
-        if pad != 0: unshuffled = F.pad(unshuffled, (pad, pad, pad, pad), value=pad_value)
-        return unshuffled.view(b,c,f,f,w//f+2*pad,h//f+2*pad).permute(0,1,2,4,3,5).reshape(b,c,w+2*f*pad, h+2*f*pad)
-
-def pixel_shuffle_up_sampling(x:torch.Tensor, f:int, pad:int=0):
-    '''
-    inverse of pixel-shuffle down-sampling (PD)
-    see more details about PD in pixel_shuffle_down_sampling()
-    Args:
-        x (Tensor) : input tensor
-        f (int) : factor of PD
-        pad (int) : number of pad will be removed
-    '''
-    # single image tensor
-    if len(x.shape) == 3:
-        c,w,h = x.shape
-        before_shuffle = x.view(c,f,w//f,f,h//f).permute(0,1,3,2,4).reshape(c*f*f,w//f,h//f)
-        if pad != 0: before_shuffle = before_shuffle[..., pad:-pad, pad:-pad]
-        return F.pixel_shuffle(before_shuffle, f)
-    # batched image tensor
-    else:
-        b,c,w,h = x.shape
-        before_shuffle = x.view(b,c,f,w//f,f,h//f).permute(0,1,2,4,3,5).reshape(b,c*f*f,w//f,h//f)
-        if pad != 0: before_shuffle = before_shuffle[..., pad:-pad, pad:-pad]
-        return F.pixel_shuffle(before_shuffle, f)
